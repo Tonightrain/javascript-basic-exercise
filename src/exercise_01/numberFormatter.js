fba251a23ef27b95b65c1c06f36a6559365aad4e
MSI-PV <805560812@qq.com>
2020-07-27 01:40:44 +0800

    complete exercise_01

--- src/exercise_01/numberFormatter.js
+++ src/exercise_01/numberFormatter.js
@@ -1,3 +1,5 @@
+import { version } from "acorn";
+
 export default function formatNumber(number, option) {
   // This function will format the number to a fixed number string. The decimal part should 2.
   // That means the number 2 will be formatted as '2.00'. The `option` is an object which contains
@@ -7,6 +9,56 @@ export default function formatNumber(number, option) {
   //
   // * Please implement the function and pass all the tests in format_number_spec.js.
   // * Please do NOT modify the signature of the function.
+ 
+  var number,numLen,dotPos;
+  var resultNumber,n;
+  var option;
+  number=""+number;
+  numLen=number.length;
+  dotPos=number.indexOf(".", 0);
 
-  throw new Error('Please delete this line and implement the function');
+  if(dotPos==-1){
+    resultNumber=number+".";
+    for(var i=0;i<2;i++){
+      resultNumber=resultNumber+"0";
+    }
+
+    if(option!=null){
+      if(option.currency==true){
+        resultNumber="$ "+resultNumber;
+      }
+    }
+
+    return resultNumber;
+  }else{
+    if((numLen-dotPos-1)>=2){
+      n=1;
+      for(var j=0;j<2;j++){
+        n=n*10;
+      }
+      resultNumber=Math.round(parseFloat(number)*n)/n;
+      resultNumber=""+resultNumber;
+
+      if(option!=null){
+        if(option.currency==true){
+          resultNumber="$ "+resultNumber;
+        }
+      }
+
+      return resultNumber;
+    }else{
+      resultNumber=number;
+      for(i=0;i<(2-numLen+dotPos+1);i++){
+        resultNumber=resultNumber+"0";
+      }
+
+      if(option!=null){
+        if(option.currency==true){
+          resultNumber="$ "+resultNumber;
+        }
+      }
+
+      return resultNumber;
+    }
+  }
 }
